07362a370fac492d65c8d785c77ab60175e9d8ba
cpan1995 <cpan1995@gmail.com>
2021-06-28 17:43:58 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(driverArray){
+    const firstTwoDrivers = () => {
+        return driverArray.slice(0,2);
+    }
+    return firstTwoDrivers();
+}
+
+function returnLastTwoDrivers(driverArray){
+    const lastTwoDrivers = () => {
+        return driverArray.slice(-2);
+    }
+    return lastTwoDrivers();
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fareMultiplier){
+    return function(currentFare){
+        return currentFare * fareMultiplier;
+    }
+}
+
+function fareDoubler(currentFare){
+    const newFare = () => {
+        return createFareMultiplier(2)(currentFare);
+    }
+    return newFare();
+}
+
+function fareTripler(currentFare){
+    const newFare = () => {
+        return createFareMultiplier(3)(currentFare);
+    }
+    return newFare();
+}
+
+function selectDifferentDrivers(drivers, unknownDrivers){
+    return unknownDrivers(drivers);
+}
